4736f8f634c5ce742ce15607876cb7de212b76f9
Adam <roboboy@kalayjian.org>
2020-04-22 23:09:22 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col } from 'react-bootstrap';
+import moment from 'moment';
+
+class Calendar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            startOfWeek: moment().startOf('week'),
+            endOfWeek: moment().endOf('week'),
+        }
+    }
+
+    render() {
+        
+        var days = [];
+        var day = this.state.startOfWeek;
+
+        while (day <= this.state.endOfWeek) {
+            days.push(day);
+            day = day.clone().add(1,'d');
+        }
+        
+        const columns = days.map((date) =>
+            <Col>
+                <div>
+                    {date.format('dddd, M/D')}
+                </div>
+            </Col>
+        );
+        
+        return (
+            <div className="calendar">
+                <Container>
+                    <Row>
+                        {columns}
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<Calendar />,
+document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
